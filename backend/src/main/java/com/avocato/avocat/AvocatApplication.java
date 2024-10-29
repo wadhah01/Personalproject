d696e88ccad4ab4ce5c35bfd94402234387edd43
Wadhah <wadhahagoubi@gmail.com>
2024-10-29 14:52:44 +0100

    first

---
+++ backend/src/main/java/com/avocato/avocat/AvocatApplication.java
@@ -0,0 +1,13 @@
+package com.avocato.avocat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvocatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvocatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/avocato/avocat/AvocatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avocato.avocat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvocatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
